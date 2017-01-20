aedda71c042e8df677dec390fe02e26598ff0bc3
Corsicanu <corsicanu22@gmail.com>
2017-01-20 12:29:44 +0200

    Script fixes

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@
 	export CROSS_COMPILE=$(pwd)/hK-tools/arm-eabi-4.8/bin/arm-eabi-
 	mkdir -p output hK-out/pack/rd hK-out/zip/hades
 
-	make -C $(pwd) O=output common_defconfig VARIANT_DEFCONFIG=fuy_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+	make -C $(pwd) O=output common_defconfig VARIANT_DEFCONFIG=fgm_defconfig SELINUX_DEFCONFIG=selinux_defconfig
 	make -j64 -C $(pwd) O=output
 
 # zImage copying - assuming the zimage is built
